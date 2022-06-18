1655540667f658c3550040dfb9b348a66c513c76
AnjanaStat <107476002+AnjanaStat@users.noreply.github.com>
2022-06-18 20:20:49 +0530

    Initial commit

---
+++ R/AMaxt3.R
@@ -0,0 +1,13 @@
+#' AMax-T test package
+#'
+#' This package find test statistic value and critical value of
+#' asymptotic Max-T test for testing
+#' equality of means against ordered alternative in one-way ANOVA model
+#' with heteroscedasticity when number of groups is 3.
+#'
+#' @docType package
+#'
+#' @author Anjana Mondal \email{mondal.anjana25@gmail.com}
+#'
+#' @name AMaxt3
+NULL
---
+++ README.md
@@ -0,0 +1,2 @@
+# AMaxt3
+ This package finds test statistics value and critical value for testing equality of treatment effects against their ordered alternatives in one-way ANOVA model when number of groups is 3
